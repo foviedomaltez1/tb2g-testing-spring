d943c22d34f7bb7044a626d1a9e0431fcd3ec4a7
foviedomaltez1 <115769400+foviedomaltez1@users.noreply.github.com>
2022-11-18 12:35:13 -0600

    Practica 14

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
 public class HearingInterpreterYannyTest {
 
+
     HearingInterpreter hearingInterpreter;
 
     @Test
--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
 class HearingInterpreterLaurelTest {
 
+
     HearingInterpreter hearingInterpreter;
 
     @Test
